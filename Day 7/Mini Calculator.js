400a503fa226d31236fd090395ab2789fdcef328
vishalNarn <vishal271615@gmail.com>
2023-06-14 16:27:06 +0530

    mini calcultor

---
+++ Day 7/Mini Calculator.js
@@ -0,0 +1,28 @@
+function add(){
+    let a=parseInt(document.getElementById('1st').value)
+    let b=parseInt(document.getElementById('2nd').value)
+    let c=a+b
+    alert(c)
+    // document.getElementById("res").innerText=c
+}
+
+function sub(){
+    let a=parseInt(document.getElementById('1st').value)
+    let b=parseInt(document.getElementById('2nd').value)
+    let c=a-b
+    alert(c)
+}
+
+function mul(){
+    let a=parseInt(document.getElementById('1st').value)
+    let b=parseInt(document.getElementById('2nd').value)
+    let c=a*b
+    alert(c)
+}
+
+function div(){
+    let a=parseInt(document.getElementById('1st').value)
+    let b=parseInt(document.getElementById('2nd').value)
+    let c=a/b
+    alert(c)
+}
