c2a015e9e9f50cb14212196788aefa63ef60f334
vishalNarn <vishal271615@gmail.com>
2023-06-14 17:46:40 +0530

    registration

--- Day 7/Mini Calculator.js
+++ Day 7/Mini Calculator.js
@@ -1,28 +1,22 @@
+a=parseInt(document.getElementById('1st').value)
+b=parseInt(document.getElementById('2nd').value)
 function add(){
-    let a=parseInt(document.getElementById('1st').value)
-    let b=parseInt(document.getElementById('2nd').value)
     let c=a+b
     alert(c)
     // document.getElementById("res").innerText=c
 }
 
 function sub(){
-    let a=parseInt(document.getElementById('1st').value)
-    let b=parseInt(document.getElementById('2nd').value)
     let c=a-b
     alert(c)
 }
 
 function mul(){
-    let a=parseInt(document.getElementById('1st').value)
-    let b=parseInt(document.getElementById('2nd').value)
     let c=a*b
     alert(c)
 }
 
 function div(){
-    let a=parseInt(document.getElementById('1st').value)
-    let b=parseInt(document.getElementById('2nd').value)
     let c=a/b
     alert(c)
 }
