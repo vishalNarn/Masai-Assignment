0d1fbb2c75b37d05c877e591b09135c8efbbe3f0
vishalNarn <vishal271615@gmail.com>
2023-06-18 01:16:24 +0530

    dfdff

--- 3 sum.py
+++ 3 sum.py
@@ -1,19 +1,21 @@
+L=[]
+A=[-1,0,1,2,-1,-4]
+A.sort()
+n=len(A)
 def f(A,n):
-    L=[]
     for i in range(n):
         j=0; k=n-1
-        while j<n and k>=0:
+        while j<n and k>=0 and i!=j and j!=k and k!=i:
             s=A[i]+A[j]+A[k]
             if s==0:
                 x=[A[i],A[j],A[k]]
-                x.sort()
                 if x not in L:
                     L.append(x)
+                    k-=1
+                    j+=1
             elif s>0:
                 k-=1
             else:
                 j+=1
     return L
-A=[-1,0,1,2,-1,-4]
-n=len(A)
 print(f(A,n))
