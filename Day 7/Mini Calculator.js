7676a66364b7cb56ff76c9f5b9858ac5cd323d6b
vishalNarn <vishal271615@gmail.com>
2023-08-19 01:46:25 +0530

    modifications

--- Day 7/Mini Calculator.js
+++ Day 7/Mini Calculator.js
@@ -1,22 +1,28 @@
-a=parseInt(document.getElementById('1st').value)
-b=parseInt(document.getElementById('2nd').value)
 function add(){
+    a=parseInt(document.getElementById('1st').value)
+    b=parseInt(document.getElementById('2nd').value)
     let c=a+b
     alert(c)
     // document.getElementById("res").innerText=c
 }
 
 function sub(){
+    a=parseInt(document.getElementById('1st').value)
+    b=parseInt(document.getElementById('2nd').value)
     let c=a-b
     alert(c)
 }
 
 function mul(){
+    a=parseInt(document.getElementById('1st').value)
+    b=parseInt(document.getElementById('2nd').value)
     let c=a*b
     alert(c)
 }
 
 function div(){
+    a=parseInt(document.getElementById('1st').value)
+    b=parseInt(document.getElementById('2nd').value)
     let c=a/b
     alert(c)
 }
